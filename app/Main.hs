1d23e2fd5b0b6c1163f40feae134d265dfb45c7f
lamekino <zoey.j.mcbride@gmail.com>
2022-07-20 01:03:37 -0400

    implemented command line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,77 @@
 import Audio
+import Control.Monad (when)
+import Data.Maybe (fromJust, fromMaybe, isNothing)
 import Parser
 import System.Environment (getArgs)
+import System.Exit
 import Types
 
-help :: String
-help = "WIP"
+data Arguements = Arguments
+  { helpFlag :: Bool,
+    audioPath :: Maybe FilePath,
+    endingTime :: Maybe Timestamp,
+    timestampFile :: Maybe FilePath,
+    outputDirectory :: Maybe FilePath,
+    songSeparator :: Maybe String
+  }
+  deriving (Eq, Show)
 
 defaultSep :: String
 defaultSep = " - "
 
--- | Takes the program arguments and gives the path to the audio and the path to
--- the timestamps
-processArgs :: [String] -> (FilePath, Maybe FilePath, Timestamp)
-processArgs args =
-  case args of
-    [x, y, z] -> (x, Just z, tsFromString y)
-    [x, y] -> (x, Nothing, tsFromString y)
-    _ -> error ("Invaild arguements " ++ unlines args)
+helpMsg :: String
+helpMsg = "Something useful"
 
--- | Takes the possible filepath and returns the parsed version
-parseInput ::
-  (FilePath -> IO String) ->
-  IO String ->
-  Maybe FilePath ->
-  IO [Metadata]
-parseInput success _ (Just fp) = parse defaultSep . tokenize <$> success fp
-parseInput _ fallback Nothing = parse defaultSep . tokenize <$> fallback
+processsArgs :: Arguements -> [String] -> Arguements
+processsArgs arg cmdline =
+  case cmdline of
+    ("-h" : _) -> arg {helpFlag = True}
+    -- TODO: is there a way to drop 'as' from this?
+    -- TODO: make this less repetitive
+    ("-o" : path : as) -> processsArgs arg {outputDirectory = Just path} as
+    ("-S" : sep : as) -> processsArgs arg {songSeparator = Just sep} as
+    ("-e" : stamp : as) -> processsArgs arg {endingTime = Just $ tsFromString stamp} as
+    ("-t" : path : as) -> processsArgs arg {timestampFile = Just path} as
+    _ -> arg
+
+-- | Takes the file contents of timestamps and titles and parses it
+parseWith :: IO String -> IO [Metadata]
+parseWith inp = parse defaultSep . tokenize <$> inp
 
 main :: IO ()
 main = do
-  (audioPath, timePath, endTime) <- processArgs <$> getArgs
+  args <-
+    processsArgs
+      Arguments
+        { helpFlag = False,
+          audioPath = Nothing,
+          endingTime = Nothing,
+          timestampFile = Nothing,
+          outputDirectory = Nothing,
+          songSeparator = Nothing
+        }
+      <$> getArgs
 
-  parsed <- parseInput readFile getContents timePath
+  -- handle help arguement
+  when (helpFlag args) $ do
+    putStrLn helpMsg
+    exitSuccess
 
-  splitFile parsed audioPath endTime
+  -- required arguments
+  let albumEnd = fromMaybe (error "required time") (endingTime args)
+  let audioFile = fromMaybe (error "required audio file") (audioPath args)
+
+  -- optional arguemnts
+  let outputDir = fromMaybe "." (outputDirectory args)
+  let songSep = fromMaybe defaultSep (songSeparator args)
+
+  -- parse from either stdin or file depending on arguemnts
+  parsed <-
+    maybe
+      (parseWith getContents)
+      (parseWith . readFile)
+      (timestampFile args)
+
+  splitFile parsed audioFile albumEnd
 
   return ()
