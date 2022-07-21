236b940a1f2f0035278e0472fe53711da87c3676
lamekino <zoey.j.mcbride@gmail.com>
2022-07-21 10:22:37 -0400

    Added exits for errors

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,21 @@
 import Audio
-import Control.Monad (when)
+import Control.Monad (unless, when)
 import Data.Maybe (fromMaybe)
 import Parser
+import System.Directory
 import System.Environment (getArgs)
 import System.Exit
+import System.IO (hPutStr, stderr)
 import Types
 
+data HelpType
+  = Pass
+  | Requested
+  | OnError
+  deriving (Eq, Show)
+
 data Arguements = Arguments
-  { helpFlag :: Bool,
+  { helpFlag :: HelpType,
     audioPath :: Maybe FilePath,
     endingTime :: Maybe Timestamp,
     timestampFile :: Maybe FilePath,
@@ -16,18 +24,25 @@ data Arguements = Arguments
   }
   deriving (Eq, Show)
 
+putErr :: String -> IO ()
+putErr = hPutStr stderr
+
 helpExit :: ExitCode -> IO ()
-helpExit code = putStrLn helpMsg >> exitWith code
+helpExit code = puts helpMsg >> exitWith code
   where
+    puts =
+      if code == ExitSuccess
+        then putStr
+        else putErr
     helpMsg =
       unlines
         [ "timesplit usage:",
-          "\t-l <timestamp in HH:MM:SS>  sets the length of the song (required).",
-          "\t-i <audio file>             the audio file to split (required).",
-          "\t-d <dir>                    set the output directory.",
-          "\t-t <txt>                    read a file with the timestamps/titles instead of stdin.",
-          "\t-S <delim>                  sets the separator between artist and song name.",
-          "\t-h                          print this help message and exit."
+          "\t-l <timestamp>   sets the length of the song (required).",
+          "\t-i <audio file>  the audio file to split (required).",
+          "\t-d <dir>         set the output directory.",
+          "\t-t <txt>         read a file with the timestamps/titles instead of stdin.",
+          "\t-S <delim>       sets the separator between artist and song name.",
+          "\t-h               print this help message and exit."
         ]
 
 defaultSep :: String
@@ -36,7 +51,7 @@ defaultSep = " - "
 processsArgs :: Arguements -> [String] -> Arguements
 processsArgs arg cmdline =
   case cmdline of
-    ("-h" : _) -> arg {helpFlag = True}
+    ("-h" : _) -> arg {helpFlag = Requested}
     -- TODO: is there a way to drop 'as' from this?
     -- TODO: make this less repetitive
     ("-S" : sep : as) -> processsArgs arg {songSeparator = Just sep} as
@@ -44,7 +59,7 @@ processsArgs arg cmdline =
     ("-i" : path : as) -> processsArgs arg {audioPath = Just path} as
     ("-l" : stamp : as) -> processsArgs arg {endingTime = Just $ tsFromString stamp} as
     ("-t" : path : as) -> processsArgs arg {timestampFile = Just path} as
-    _ -> arg
+    _ -> arg {helpFlag = OnError}
 
 -- | Takes the file contents of timestamps and titles and parses it
 parseInput :: String -> IO String -> IO [Metadata]
@@ -54,9 +69,8 @@ main :: IO ()
 main = do
   args <-
     processsArgs
-      -- default arguements
       Arguments
-        { helpFlag = False,
+        { helpFlag = Pass,
           audioPath = Nothing,
           endingTime = Nothing,
           timestampFile = Nothing,
@@ -66,18 +80,30 @@ main = do
       <$> getArgs
 
   -- handle help arguement
-  print args
-
-  when (helpFlag args) $
+  when (helpFlag args == Requested) $
     helpExit ExitSuccess
+
+  when (helpFlag args == OnError) $
+    helpExit $ ExitFailure 1
 
   -- required arguments
   let albumEnd = fromMaybe (error "required time") (endingTime args)
       audioFile = fromMaybe (error "required audio file") (audioPath args)
 
   -- optional arguemnts
-  let outputDir = fromMaybe "." (outputDirectory args)
+  let outputDir = fromMaybe "." (outputDirectory args) ++ "/"
       songSep = fromMaybe defaultSep (songSeparator args)
+
+  -- file checks
+  -- TODO: bind this with unless
+  outputExists <- doesDirectoryExist outputDir
+  inputExists <- doesFileExist audioFile
+
+  unless outputExists $
+    die $ "directroy does not exist: " ++ outputDir
+
+  unless inputExists $
+    die $ "audio does not exist: " ++ audioFile
 
   -- parse from either stdin or file depending on arguemnts
   parsed <-
@@ -87,6 +113,4 @@ main = do
           (parseWith . readFile)
           (timestampFile args)
 
-  return ()
-
--- splitFile parsed audioFile albumEnd
+  splitFile parsed outputDir audioFile albumEnd
